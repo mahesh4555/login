f0a0632cb0d0d12782af760f1ce2df6466b825e0
Mahesh <mahesh4k55@gmail.com>
2020-12-07 16:19:44 +0530

    Added code for google authentication 'google-auth-library'

--- backend_server/auth/AuthController.js
+++ backend_server/auth/AuthController.js
@@ -1,4 +1,4 @@
-var express = require("express");
+var  express = require("express");
 var router = express.Router();
 var bodyParser = require("body-parser");
 router.use(bodyParser.urlencoded({ extended: false }));
@@ -10,6 +10,7 @@ const { verifyToken1 } = require("./VerifyToken");
 var jwt = require("jsonwebtoken");
 var bcrypt = require("bcryptjs");
 var config = require("../config");
+const {OAuth2Client} = require('google-auth-library');
 
 router.post("/register", function (req, res) {
   console.log("POST regster");
@@ -98,4 +99,61 @@ router.post("/login", function (req, res) {
   // res.status(200).send({ auth: true, token: "token" });
 });
 
+
+
+router.post("/google_login",  function (req, res) {
+  console.log("POST login");
+  //  console.log("name : ",req.body.password);
+  console.log(req.body);
+  console.log("email:", req.body.email);
+  // console.log("password:", req.body.password);
+
+  // User.findOne({ email: req.body.email },async function (err, user) {
+  //   if (err) return res.status(500).send({ info: "Error on the server." });
+  //   if (!user) return res.status(404).send({ info: "No user found" });
+
+    // var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+    // if (!passwordIsValid)
+    //   return res.status(401).send({ auth: false, token: null });
+  
+   
+    const client = new OAuth2Client("874607787308-r3vkocggamqukjvfeg1t3f13pc6c3abu.apps.googleusercontent.com");
+
+
+     
+    console.log("client:",client)
+    const ticket = await client.verifyIdToken({
+      idToken : req.body.token,
+      audience : "874607787308-r3vkocggamqukjvfeg1t3f13pc6c3abu.apps.googleusercontent.com"
+      });
+      console.log("ticket:",ticket)
+
+      const payload = ticket.getPayload();
+      console.log("payload:",payload)
+      const userDetails = {
+      email : payload['email'],
+      firstname : payload['given_name'],
+  
+      }
+      console.log("userDetails:",userDetails)
+
+      // var token = jwt.sign(userDetails, config.secret, {expiresIn: 1440});
+
+       if(payload['email_verified'])
+       {
+        var token = jwt.sign({ id: userDetails['email'] }, config.secret, {
+          expiresIn: 86400, // expires in 24 hours
+        });
+        console.log("Success");
+    
+        res.status(200).send({ auth: true, token: token });
+       }
+       
+       res.status(200).send({ auth: false, token: null });
+    
+  });
+  // res.status(200).send({ auth: true, token: "token" });
+
 module.exports = router;
+
+
